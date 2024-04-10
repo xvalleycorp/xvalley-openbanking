7396a2d149460b031ccc7d26da11af2e66d4d09b
Steve VU <steve@xvalley.top>
2024-04-10 09:55:26 +0700

    Update the project structure

---
+++ src/main/java/com/xvalley/openbanking/BianOpenBankingDemoApplication.java
@@ -0,0 +1,24 @@
+package com.xvalley.openbanking;
+
+import com.sun.tools.javac.Main;
+import com.xvalley.openbanking.controller.ContactHandlerController;
+import com.xvalley.openbanking.models.persistence.DemoPersistence;
+import com.xvalley.openbanking.models.persistence.IPersistence;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BianOpenBankingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BianOpenBankingDemoApplication.class, args);
+	}
+
+	@Bean
+	public IPersistence persistence(){
+		return new DemoPersistence();
+	}
+}
---
+++ src/test/java/com/xvalley/openbanking/BianOpenBankingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xvalley.openbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BianOpenBankingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
